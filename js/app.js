4301a7812b0b5caa43b719f836b3a28603ea78d9
Sierra <Parkersierra40@gmail.com>
2026-08-15 15:04:07 -0400

    Added contact form and fixed project.html

--- js/app.js
+++ js/app.js
@@ -5,12 +5,139 @@ contactBtn2.addEventListener("click", openContact);
 
 const contactPanel = document.getElementById('contactPanel')
 
-const closeBtn =document.getElementById('closePanel')
-closeBtn.addEventListener("click", closeContact )
+const closeBtn = document.getElementById('closePanel')
+closeBtn.addEventListener("click", closeContact)
 
 function openContact() {
     contactPanel.classList.add("active")
 }
-function closeContact () {
+function closeContact() {
     contactPanel.classList.remove("active")
 }
+
+const projects = [
+    {
+        projectName: 'The Foodie Leonne Blog',
+        projectImage: 'assets/TFL.png',
+        technologies: [{
+            name: 'HTML',
+            icon: 'assets/icons/html.png'
+        },
+        {
+            name: 'CSS',
+            icon: 'assets/icons/css.png'
+        },
+        {
+            name: 'JavaScript',
+            icon: 'assets/icons/javascript.png'
+        }],
+        projectDescription: 'A responsive food blog featuring recipes, restaurant reviews, and desserts, with a focus on clean design and user-friendly navigation.',
+        liveDemo: '',
+        github: ''
+    },
+    {
+        projectName: '',
+        projectImage: '',
+        technologies: [
+            {
+                name: 'HTML',
+                icon: 'assets/icons/html.png'
+            },
+            {
+                name: 'CSS',
+                icon: 'assets/icons/css.png'
+            },
+            {
+                name: 'JavaScript',
+                icon: 'assets/icons/javascript.png'
+            }
+        ],
+        projectDescription: '',
+        liveDemo: '',
+        github: ''
+    },
+    {
+        projectName: '',
+        projectImage: '',
+        technologies: [
+            {
+                name: 'HTML',
+                icon: 'assets/icons/html.png'
+            },
+            {
+                name: 'CSS',
+                icon: 'assets/icons/css.png'
+            },
+            {
+                name: 'JavaScript',
+                icon: 'assets/icons/javascript.png'
+            }
+        ],
+        projectDescription: '',
+        liveDemo: '',
+        github: ''
+    },
+        {
+        projectName: '',
+        projectImage: '',
+        technologies: [
+            {
+                name: 'HTML',
+                icon: 'assets/icons/html.png'
+            },
+            {
+                name: 'CSS',
+                icon: 'assets/icons/css.png'
+            },
+            {
+                name: 'JavaScript',
+                icon: 'assets/icons/javascript.png'
+            }
+        ],
+        projectDescription: '',
+        liveDemo: '',
+        github: ''
+    },
+        {
+        projectName: '',
+        projectImage: '',
+        technologies: [
+            {
+                name: 'HTML',
+                icon: 'assets/icons/html.png'
+            },
+            {
+                name: 'CSS',
+                icon: 'assets/icons/css.png'
+            },
+            {
+                name: 'JavaScript',
+                icon: 'assets/icons/javascript.png'
+            }
+        ],
+        projectDescription: '',
+        liveDemo: '',
+        github: ''
+    },
+        {
+        projectName: '',
+        projectImage: '',
+        technologies: [
+            {
+                name: 'HTML',
+                icon: 'assets/icons/html.png'
+            },
+            {
+                name: 'CSS',
+                icon: 'assets/icons/css.png'
+            },
+            {
+                name: 'JavaScript',
+                icon: 'assets/icons/javascript.png'
+            }
+        ],
+        projectDescription: '',
+        liveDemo: '',
+        github: ''
+    },
+]
