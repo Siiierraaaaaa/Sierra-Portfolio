eb1b551e13f7cbb162c3c4e3de08f75956886190
Sierra <Parkersierra40@gmail.com>
2026-08-15 15:04:50 -0400

    Added contact form and fixed project.html

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const contactBtn = document.getElementById('contactBtn');
-const contactBtn2 = document.getElementById("contactBtn2");
 contactBtn.addEventListener("click", openContact);
-contactBtn2.addEventListener("click", openContact);
+
 
 const contactPanel = document.getElementById('contactPanel')
 
@@ -36,8 +35,33 @@ const projects = [
         github: ''
     },
     {
-        projectName: '',
-        projectImage: '',
+        projectName: 'The Foodie Leonne | WordPress Food Blog',
+        projectImage: 'assets/TFL-WordPress.png',
+        technologies: [
+            {
+                name: 'WordPress',
+                icon: 'assets/icons/wp.png'
+            },
+            {
+                name: 'Gutenberg',
+                icon: 'assets/icons/gutenberg.png'
+            },
+            {
+                name: 'HTML',
+                icon: 'assets/icons/html.png'
+            },
+            {
+                name: 'CSS',
+                icon: 'assets/icons/css.png'
+            }
+        ],
+        projectDescription: 'A WordPress version of my original food blog, customized with Gutenberg, HTML, and CSS. ',
+        liveDemo: 'https://sierra-parker.webhostingforstudents.com/classes/2510/',
+        github: ''
+    },
+    {
+        projectName: 'Brew Haven Coffee',
+        projectImage: 'assets/Brew-Haven-Coffee.png',
         technologies: [
             {
                 name: 'HTML',
@@ -52,13 +76,34 @@ const projects = [
                 icon: 'assets/icons/javascript.png'
             }
         ],
-        projectDescription: '',
+        projectDescription: 'A responsive coffee shop website designed to create a warm, welcoming experience for customers. Features include a featured drinks section, customer testimonials, location map, interactive pop-ups, and a structured navigation system.',
+        liveDemo: 'https://brew-haven-coffeee-jchd.vercel.app/',
+        github: 'https://github.com/Siiierraaaaaa/brew-haven-coffeee'
+    },
+    {
+        projectName: 'Color Changing App',
+        projectImage: 'assets/Color-Changing-App.png',
+        technologies: [
+            {
+                name: 'HTML',
+                icon: 'assets/icons/html.png'
+            },
+            {
+                name: 'CSS',
+                icon: 'assets/icons/css.png'
+            },
+            {
+                name: 'JavaScript',
+                icon: 'assets/icons/javascript.png'
+            }
+        ],
+        projectDescription: 'An interactive JavaScript application that allows users to change the background color with a button click. Built to practice JavaScript events, functions, DOM manipulation, and dynamic styling.',
         liveDemo: '',
         github: ''
     },
     {
-        projectName: '',
-        projectImage: '',
+        projectName: 'Counter App',
+        projectImage: 'assets/Count-App.png',
         technologies: [
             {
                 name: 'HTML',
@@ -73,71 +118,63 @@ const projects = [
                 icon: 'assets/icons/javascript.png'
             }
         ],
-        projectDescription: '',
-        liveDemo: '',
-        github: ''
-    },
-        {
-        projectName: '',
-        projectImage: '',
-        technologies: [
-            {
-                name: 'HTML',
-                icon: 'assets/icons/html.png'
-            },
-            {
-                name: 'CSS',
-                icon: 'assets/icons/css.png'
-            },
-            {
-                name: 'JavaScript',
-                icon: 'assets/icons/javascript.png'
-            }
-        ],
-        projectDescription: '',
-        liveDemo: '',
-        github: ''
-    },
-        {
-        projectName: '',
-        projectImage: '',
-        technologies: [
-            {
-                name: 'HTML',
-                icon: 'assets/icons/html.png'
-            },
-            {
-                name: 'CSS',
-                icon: 'assets/icons/css.png'
-            },
-            {
-                name: 'JavaScript',
-                icon: 'assets/icons/javascript.png'
-            }
-        ],
-        projectDescription: '',
-        liveDemo: '',
-        github: ''
-    },
-        {
-        projectName: '',
-        projectImage: '',
-        technologies: [
-            {
-                name: 'HTML',
-                icon: 'assets/icons/html.png'
-            },
-            {
-                name: 'CSS',
-                icon: 'assets/icons/css.png'
-            },
-            {
-                name: 'JavaScript',
-                icon: 'assets/icons/javascript.png'
-            }
-        ],
-        projectDescription: '',
+        projectDescription: 'An interactive JavaScript application that allows users to increase, decrease, and reset a counter  while demonstrating DOM manipulation, event handling, and responsive design.',
         liveDemo: '',
         github: ''
     },
 ]
+// declaring that projectItem is selecting html class projects
+const projectItem = document.querySelector('.projects')
+
+
+
+
+function displayProjects() {
+
+    projectItem.innerHTML = projects.map(project => {
+
+        const technologiesHTML = project.technologies.map(technology => {
+            return `
+                <div class="project-technology">
+                    <h5>${technology.name}</h5>
+                    <img src="${technology.icon}" alt="${technology.name}">
+                </div>
+            `;
+        }).join('');
+
+        return `
+            <div class="project-card">
+
+                <img src="${project.projectImage}" alt="${project.projectName}">
+
+                <div class="project-description">
+
+                    <h4>${project.projectName}</h4>
+
+                    <p>${project.projectDescription}</p>
+
+                    <h5>Built With:</h5>
+
+                    <div class="project-technologies">
+                        ${technologiesHTML}
+                    </div>
+
+                    <div class="project-links">
+                        <a href="${project.liveDemo}" target="_blank" class="btn">
+                            Live Demo
+                        </a>
+
+                        <a href="${project.github}" target="_blank" class="btn">
+                            Github
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
+        `;
+
+    }).join('');
+}
+
+displayProjects();
